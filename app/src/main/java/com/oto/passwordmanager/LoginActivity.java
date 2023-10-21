6d9baa576a20b25a0e7aba60a5c4f5135b882c04
Siddhes Dupare <siddheshdupare.sd2004@gmail.com>
2023-10-21 14:56:06 +0530

    Changes for LoginActivity

--- app/src/main/java/com/oto/passwordmanager/LoginActivity.java
+++ app/src/main/java/com/oto/passwordmanager/LoginActivity.java
@@ -1,14 +1,58 @@
 package com.oto.passwordmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private ProgressBar progressBar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailEditText = findViewById(R.id.login_email);
+        passwordEditText = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_login);
+        progressBar = findViewById(R.id.login_progressbar);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar.setVisibility(View.GONE);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                loginAuthentication();
+            }
+        });
+    }
+    private void loginAuthentication() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
